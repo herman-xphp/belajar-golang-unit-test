2e90e461b07db45bb1e5718dfa13dfcd96055fb7
herman-xphp <hermanxphp@gmail.com>
2025-09-29 13:19:41 +0800

    table test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -9,6 +9,47 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestTableHelloWorld(t *testing.T) {
+	tests := []struct {
+		name     string
+		request  string
+		expected string
+	}{
+		{
+			name:     "Eko",
+			request:  "Eko",
+			expected: "Hello Eko",
+		},
+		{
+			name:     "Kurniawan",
+			request:  "Kurniawan",
+			expected: "Hello Kurniawan",
+		},
+		{
+			name:     "Khannedy",
+			request:  "Khannedy",
+			expected: "Hello Khannedy",
+		},
+		{
+			name:     "Budi",
+			request:  "Budi",
+			expected: "Hello Budi",
+		},
+		{
+			name:     "Joko",
+			request:  "Joko",
+			expected: "Hello Joko",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := HelloWorld(test.request)
+			require.Equal(t, test.expected, result)
+		})
+	}
+}
+
 func TestSubTest(t *testing.T) {
 	t.Run("Ucok", func(t *testing.T) {
 		result := HelloWorld("Ucup")
