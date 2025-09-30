9bc82dace0136c204ed74fc3476ed3cc1b7b488c
herman-xphp <hermanxphp@gmail.com>
2025-09-30 11:43:21 +0800

    sub benchmark

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -9,6 +9,19 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func BenchmarkSub(b *testing.B) {
+	b.Run("Eko", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HelloWorld("Eko")
+		}
+	})
+	b.Run("Anto", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HelloWorld("Anto")
+		}
+	})
+}
+
 func BenchmarkHelloWorldEko(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		HelloWorld("Eko")
